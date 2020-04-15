4adc8440c030fcf3ab98d272bb4ad4a4bfd05ab8
Dennis <dennsva@gmail.com>
2020-04-15 22:03:56 +0300

    Tehtävä 4

--- blogilista/tests/list_helper.test.js
+++ blogilista/tests/list_helper.test.js
@@ -1,8 +1,26 @@
 const listHelper = require('../utils/list_helper')
 
-test('dummy returns one', () => {
-  const blogs = []
+describe('dummy', () => {
+  const emptyList = []
 
-  const result = listHelper.dummy(blogs)
-  expect(result).toBe(1)
+  test('dummy returns one', () => {
+    const result = listHelper.dummy(emptyList)
+    expect(result).toBe(1)
+  })
+})
+
+describe('totalLikes', () => {
+  const exampleBlogs = require('./example_blogs')
+
+  test('empty list', () => {
+    expect(listHelper.totalLikes([])).toBe(0)
+  })
+
+  test('list with one blog', () => {
+    expect(listHelper.totalLikes(exampleBlogs.listWithOneBlog)).toBe(5)
+  })
+
+  test('example blog list with six blogs', () => {
+    expect(listHelper.totalLikes(exampleBlogs.listWithSixBlogs)).toBe(36)
+  })
 })
